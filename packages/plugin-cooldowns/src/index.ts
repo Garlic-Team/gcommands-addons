9b03b131ee26c783972bb326cd8967998108d3f1
xhyrom <generalkubo@gmail.com>
2022-01-08 16:21:56 +0100

    fix typings

--- packages/plugin-cooldowns/src/index.ts
+++ packages/plugin-cooldowns/src/index.ts
@@ -4,3 +4,6 @@ import { Cooldowns } from './utils/CooldownManager';
 new Plugin('@gcommands/plugin-cooldowns', () => {
 	Cooldowns.init();
 });
+
+export * from './inhibitors/CooldownInhibitor';
+export * from './utils/CooldownManager';
