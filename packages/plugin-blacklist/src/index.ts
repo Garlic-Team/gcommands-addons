b61a1f1ecdfba615fea8b3b808d04e1d9cb38a2b
xhyrom <generalkubo@gmail.com>
2022-01-09 10:10:47 +0100

    blacklist plugin

--- packages/plugin-blacklist/src/index.ts
+++ packages/plugin-blacklist/src/index.ts
@@ -1,9 +1,17 @@
-import { Logger, Plugin } from 'gcommands';
+import { Command, Logger, Plugin } from 'gcommands';
+import { BlacklistInhibitor } from './inhibitors/BlacklistInhibitor';
+import { BlacklistManager } from './utils/BlacklistManager';
 
 const pluginName = '@gcommands/plugin-blacklist'
+
+Command.setDefaults({
+	inhibitors: [
+		new BlacklistInhibitor()
+	]
+})
 
 new Plugin(pluginName, (client) => {
 	if (!client.getDatabase()) return Logger.error('Please add the database parameter to the client.', pluginName);
 
-	console.log('no empty')
+	BlacklistManager.init();
 });
---
+++ packages/plugin-blacklist/src/utils/BlacklistManager.ts
@@ -0,0 +1,54 @@
+import { LimitedCollection, Snowflake } from 'discord.js';
+import { GClient } from 'gcommands';
+
+export class BlacklistManagerClass {
+    cache: LimitedCollection<Snowflake, boolean>;
+
+    constructor() {
+        this.cache = new LimitedCollection({ maxSize: 100 });
+    }
+
+	init(): void {
+		return;
+	}
+
+	async hasBlacklist(client: GClient, userId: Snowflake): Promise<boolean> {
+		const has = await this._getBlacklist(client, userId);
+
+		return has;
+	}
+
+	async setBlacklist(client: GClient, userId: Snowflake, blacklist: boolean) {
+		this._setBlacklist(client, userId, blacklist);
+	}
+
+	private async _getBlacklist(client: GClient, userId: Snowflake): Promise<boolean> {
+		const db = (client.getDatabase() as any);
+
+        if (String(this.cache.get(userId)) !== 'undefined') return this.cache.get(userId);
+
+		if (db.type === 'mongodb') {
+			return (await db.get('plugin-blacklist', { userId }))?.blacklisted;
+		} else if (db.type === 'prismaio') {
+			return (await db.get('plugin-blacklist', { userId }))?.blacklisted;
+		} else {
+			return (await db.get(`plugin-blacklist-${userId}}`));
+		}
+	}
+
+	private async _setBlacklist(client: GClient, userId: Snowflake, blacklisted: boolean) {
+		const db = (client.getDatabase() as any);
+
+        this.cache.set(userId, blacklisted);
+
+		if (db.type === 'mongodb') {
+			return await db.insert('plugin-blacklist', { userId, blacklisted });
+		} else if (db.type === 'prismaio') {
+			return await db.insert('plugin-blacklist', { userId, blacklisted });
+		} else {
+			return await db.insert(`plugin-blacklist-${userId}}`, blacklisted);
+		}
+	}
+}
+
+export const BlacklistManager = new BlacklistManagerClass();
