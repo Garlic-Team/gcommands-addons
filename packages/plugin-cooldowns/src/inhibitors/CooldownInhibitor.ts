a05e04140c12529b208d92e0a8f5357f0091fd52
xhyrom <generalkubo@gmail.com>
2022-01-22 11:36:37 +0100

    feat: whitelist in cooldown inhibitor

--- packages/plugin-cooldowns/src/inhibitors/CooldownInhibitor.ts
+++ packages/plugin-cooldowns/src/inhibitors/CooldownInhibitor.ts
@@ -1,20 +1,33 @@
 import { CommandContext, ComponentContext, Inhibitor } from 'gcommands';
 import { Cooldowns } from '../utils/CooldownManager';
 import ms from 'ms';
+import { Snowflake } from 'discord.js';
 
 export interface CooldownInhibitorOptions extends Inhibitor.InhibitorOptions {
+	whitelist?: Array<Snowflake>;
+	getWhitelist? (ctx: CommandContext | ComponentContext): Array<Snowflake>;
 	cooldown: string;
 }
 
 export class CooldownInhibitor extends Inhibitor.Inhibitor {
-	cooldown: number;
+	public whitelist?: Array<Snowflake>;
+	public getWhitelist? (ctx: CommandContext | ComponentContext): Array<Snowflake>;
+	public readonly cooldown: number;
+
 	constructor(options: CooldownInhibitorOptions) {
 		super(options);
     
+		this.whitelist = options.whitelist;
+		this.getWhitelist = options.getWhitelist;
 		this.cooldown = ms(options.cooldown);
 	}
 
 	async run(ctx: CommandContext | ComponentContext): Promise<any> {
+		const dynamicWhitelist = this.getWhitelist?.(ctx);
+		if (dynamicWhitelist) this.whitelist = dynamicWhitelist;
+
+		if (this.whitelist.includes(ctx.userId)) return true;
+
 		const hasCooldown = await Cooldowns.hasCooldown(ctx.client, ctx.userId);
 		if (hasCooldown) {
 			return ctx.reply({
