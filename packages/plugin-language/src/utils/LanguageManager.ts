eb196800852faf921756022efd456174def8dd73
xhyrom <generalkubo@gmail.com>
2022-02-10 16:47:03 +0100

    fix(plugin-language): some things

--- packages/plugin-language/src/utils/LanguageManager.ts
+++ packages/plugin-language/src/utils/LanguageManager.ts
@@ -1,8 +1,8 @@
-import { CommandInteraction, ContextMenuInteraction } from 'discord.js';
+import { Client, Interaction } from 'discord.js';
 import { GClient } from 'gcommands';
 
 export class LanguageManager {
-    public static __(i: CommandInteraction | ContextMenuInteraction, name: string) {
+    public static __(i: { client: Client | GClient, locale?: string, guildLocale?: string } | Interaction, name: string) {
         const client = i.client as GClient;
         const language = i?.locale || i?.guildLocale || client.__lang__.defaultLanguage;
         
