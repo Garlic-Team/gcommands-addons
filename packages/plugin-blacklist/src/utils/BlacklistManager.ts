0646b999f8f4a87121e11c7475d78023349d1ac6
xhyrom <generalkubo@gmail.com>
2022-05-15 13:49:41 +0200

    fix: use type

--- packages/plugin-blacklist/src/utils/BlacklistManager.ts
+++ packages/plugin-blacklist/src/utils/BlacklistManager.ts
@@ -1,5 +1,5 @@
 import { LimitedCollection, Snowflake } from 'discord.js';
-import { GClient } from 'gcommands';
+import type { GClient } from 'gcommands';
 
 export class BlacklistManagerClass {
 	cache: LimitedCollection<Snowflake, boolean>;
