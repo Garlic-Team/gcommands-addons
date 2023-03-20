7d9ea95d2c7b6b7fce282eccbc5c5c078956a1d8
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2023-03-20 21:35:06 +0100

    feat+fix!: Even more events added
    Updated to use discord.js v14 lib and added new events.

--- packages/plugin-moreevents/src/listeners/interactions.ts
+++ packages/plugin-moreevents/src/listeners/interactions.ts
@@ -8,10 +8,16 @@ new Listener({
 
 		if (interaction.isButton()) {
 			client.emit('clickButton', interaction);
-		} else if (interaction.isSelectMenu()) {
+		} else if (interaction.isAnySelectMenu()) {
 			client.emit('selectMenu', interaction);
 		} else if (interaction.isAutocomplete()) {
 			client.emit('autocomplete', interaction);
+		} else if (interaction.isChatInputCommand()) {
+			client.emit('applicationCommand', interaction);
+		} else if (interaction.isMessageContextMenuCommand()) {
+			client.emit('messageContextMenuCommand', interaction);
+		} else if (interaction.isUserContextMenuCommand()) {
+			client.emit('userContextMenuCommand', interaction);
 		}
 	},
 });
