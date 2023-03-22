4f0f931a78d008f9665e23c7c3e1b96d67c67d79
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2023-03-23 00:03:29 +0100

    feat: Even more events added

--- packages/plugin-moreevents/src/index.ts
+++ packages/plugin-moreevents/src/index.ts
@@ -8,12 +8,20 @@ declare module 'discord.js' {
 	interface ClientEvents {
 		selectMenu: [SelectMenuInteraction];
 		clickButton: [ButtonInteraction];
+		autocomplete: [AutocompleteInteraction];
+		applicationCommand: [ChatInputCommandInteraction];
+		messageContextMenuCommand: [MessageContextMenuCommandInteraction];
+		userContextMenuCommand: [UserContextMenuCommandInteraction];
 
+		guildLargeUpdate: [Guild, boolean, boolean];
 		guildBoostLevelUp: [Guild, string, string];
 		guildBoostLevelDown: [Guild, string, string];
-		guildRegionUpdate: [Guild, string, string];
 		guildBannerUpdate: [Guild, string, string];
-		guildAfkChannelUpdate: [Guild, Channel, Channel];
+		guildAfkChannelUpdate: [Guild, GuildChannel, GuildChannel];
+		guildModeratorNewsChannelUpdate: [Guild, GuildChannel, GuildChannel];
+		guildRulesChannelUpdate: [Guild, GuildChannel, GuildChannel];
+		guildSystemChannelUpdate: [Guild, GuildChannel, GuildChannel];
+		guildSystemEventsUpdate: [Guild, Readonly<SystemChannelFlagsBitField>, Readonly<SystemChannelFlagsBitField>];
 		guildVanityURLUpdate: [Guild, string, string];
 		guildFeaturesUpdate: [Guild, object, object];
 		guildAcronymUpdate: [Guild, string, string];
@@ -21,7 +29,29 @@ declare module 'discord.js' {
 		guildMaximumMembersUpdate: [Guild, number, number];
 		guildPartnerUpdate: [Guild, boolean, boolean];
 		guildVerifyUpdate: [Guild, boolean, boolean];
-
+		guildNameUpdate: [Guild, string, string];
+		guildIconUpdate: [Guild, Guild];
+		guildDiscoverySplashUpdate: [Guild, Guild];
+		guildSplashUpdate: [Guild, Guild];
+		guildDescriptionUpdate: [Guild, string, string];
+		guildDefaultMessageNotificationsUpdate: [Guild, GuildDefaultMessageNotifications, GuildDefaultMessageNotifications];
+		guildExplicitContentFilterUpdate: [Guild, GuildExplicitContentFilter, GuildExplicitContentFilter];
+		guildMaximumMembersIncreased: [Guild, number, number];
+		guildMaximumMembersDecreased: [Guild, number, number];
+		guildMaxVideoChannelUsersIncreased: [Guild, number, number];
+		guildMaxVideoChannelUsersDecreased: [Guild, number, number];
+		guildMFAProtectionDisabled: [Guild, GuildMFALevel, GuildMFALevel];
+		guildMFAProtectionEnabled: [Guild, GuildMFALevel, GuildMFALevel];
+		guildNSFWLevelDecreased: [Guild, GuildNSFWLevel, GuildNSFWLevel];
+		guildNSFWLevelIncreased: [Guild, GuildNSFWLevel, GuildNSFWLevel];
+		guildVerificationLevelIncreased: [Guild, GuildVerificationLevel, GuildVerificationLevel];
+		guildVerificationLevelDecreased: [Guild, GuildVerificationLevel, GuildVerificationLevel];
+		guildLocaleUpdate: [Guild, Locale, Locale];
+		guildBoostProgressBarUpdate: [Guild, boolean, boolean];
+		guildShardUpdate: [Guild, WebSocketShard, WebSocketShard];
+		guildWidgetUpdate: [Guild, boolean, boolean];
+		guildWidgetChannelUpdate: [Guild, GuildChannel, GuildChannel];
+		
 		threadStateUpdate: [ThreadChannel, ThreadChannel];
 		threadNameUpdate: [ThreadChannel, string, string];
 		threadLockStateUpdate: [ThreadChannel, ThreadChannel];
@@ -41,14 +71,19 @@ declare module 'discord.js' {
 		voiceChannelUndeaf: [GuildMember, VoiceChannelDeafType];
 		voiceStreamingStart: [GuildMember, VoiceBasedChannel];
 		voiceStreamingStop: [GuildMember, VoiceBasedChannel];
-
-		guildMemberNicknameUpdate: [GuildMember, string, string];
-		guildMemberAcceptShipScreening: [GuildMember];
+		voiceCameraDisabled: [GuildMember, VoiceBasedChannel];
+		voiceCameraEnabled: [GuildMember, VoiceBasedChannel];
+		
 		guildMemberBoost: [GuildMember, Date, Date];
 		guildMemberUnboost: [GuildMember, Date, Date];
+		guildMemberNicknameUpdate: [GuildMember, string, string];
 		guildMemberTimeoutAdded: [GuildMember, Date, Date];
-		guildMemberTimeoutChanged: [GuildMember, Date, Date];
 		guildMemberTimeoutRemoved: [GuildMember, Date, Date];
+		guildMemberTimeoutUpdate: [GuildMember, Date, Date];
+		guildMemberAcceptShipScreening: [GuildMember];
+		guildMemberRoleUpdate: [GuildMember, GuildMemberRoleManager, GuildMemberRoleManager];
+		guildMemberGuildAvatarUpdate: [GuildMember, string, string];
+		guildMemberFlagsUpdate: [GuildMember, Readonly<GuildMemberFlagsBitField>, Readonly<GuildMemberFlagsBitField>];
 
 		userAvatarUpdate: [User, string, string];
 		userUsernameUpdate: [User, string, string];
@@ -58,6 +93,13 @@ declare module 'discord.js' {
 
 		rolePositionUpdate: [Role, number, number];
 		rolePermissionsUpdate: [Role, bigint, bigint];
+		roleColorUpdate: [Role, string, string];
+		roleEditUpdate: [Role, boolean, boolean];
+		roleHoistUpdate: [Role, boolean, boolean];
+		roleIconUpdate: [Role, string, string];
+		roleManageUpdate: [Role, boolean, boolean];
+		roleMentionUpdate: [Role, boolean, boolean];
+		roleNameUpdate: [Role, string, string];
 	}
 }
 
